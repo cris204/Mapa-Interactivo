bc754fbc180f15ce940adddd106b5d62bc2e133e
cristian <crist.1707@hotmail.com>
2018-05-17 00:47:43 -0500

    inicio

---
+++ Assets/Scripts/Activar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activar : MonoBehaviour {
+
+    public GameObject[] activar;
+    public GameObject[] desactivar;
+
+    
+    public void ActivarCositas()
+    {
+        for (int i = 0; i < activar.Length; i++)
+        {
+            activar[i].SetActive(true);
+        }
+    }
+
+    public void DesactivarCositas()
+    {
+        for (int i = 0; i < desactivar.Length; i++)
+        {
+            desactivar[i].SetActive(false);
+        }
+    }
+
+}
