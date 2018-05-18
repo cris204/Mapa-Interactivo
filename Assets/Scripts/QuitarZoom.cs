edafa8f2f0fb6373b917570b7441aa209c7793de
cristian <crist.1707@hotmail.com>
2018-05-18 00:59:39 -0500

    falta usos y el otro

---
+++ Assets/Scripts/QuitarZoom.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class QuitarZoom : MonoBehaviour,IPointerDownHandler {
+
+    public ZoomFotos zoom;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        zoom.Zoom();
+    }
+}
---
+++ Assets/Scripts/ZoomFotos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ZoomFotos : MonoBehaviour
+{
+
+    public GameObject imagenZoom;
+    public GameObject imagenAtras;
+    public bool zoom;
+
+    public void Zoom()
+    {
+        if (!zoom)
+        {
+            imagenZoom.SetActive(true);
+            imagenAtras.SetActive(true);
+            zoom = true;
+        }
+        else
+        {
+            imagenAtras.SetActive(false);
+            imagenZoom.SetActive(false);
+            zoom = false;
+        }
+    }
+    
+
+
+}
